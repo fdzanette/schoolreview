e5e0f3647f9ce46171b5410d42b949f53744ea33
fabricio zanette <fabzanette@gmail.com>
2018-06-25 18:01:46 -0300

    set marker to map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -2,15 +2,9 @@ import GMaps from 'gmaps/gmaps.js';
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const mark = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
-  mark.addMarkers(markers);
-  if (markers.length === 0) {
-    mark.setZoom(2);
-  } else if (markers.length === 1) {
-    mark.setCenter(markers[0].lat, markers[0].lng);
-    mark.setZoom(14);
-  } else {
-    mark.fitLatLngBounds(markers);
-  }
+  map.addMarker(markers);
+  map.setCenter(markers["lat"], markers["lng"]);
+  map.setZoom(16);
 }
