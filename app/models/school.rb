19fee3932033ad28b781b5b3ab15dcfa474f3922
fabricio zanette <fabzanette@gmail.com>
2018-06-27 10:14:22 -0300

    add city to school model

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,4 +3,7 @@ class School < ApplicationRecord
   has_many :photos, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
 end
