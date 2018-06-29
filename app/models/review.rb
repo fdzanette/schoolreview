c24bdd4eb9cfa1814ee1e33b5b37c89cf589bd65
fabricio zanette <fabzanette@gmail.com>
2018-06-29 10:32:26 -0300

    add links to photo posts

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :school
   belongs_to :user
-  validates :content, presence: true
   validates :rating, :inclusion => { :in => 0..5 }
   mount_uploader :photo, PhotoUploader
 end
