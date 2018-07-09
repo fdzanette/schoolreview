d2b766f1caa7eea042f3af3baaf134b9fdf62612
fabricio zanette <fabzanette@gmail.com>
2018-07-09 14:49:16 -0300

    drop migration dropschoolpictures

--- db/migrate/20180709172529_drop_school_pictures_table.rb
+++
@@ -1,9 +0,0 @@
-class DropSchoolPicturesTable < ActiveRecord::Migration[5.1]
-  def up
-    drop_table :school_pictures
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
