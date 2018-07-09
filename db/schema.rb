4ee8e8cc446bd6c563faf8cebed7e04037fd8519
fabricio zanette <fabzanette@gmail.com>
2018-07-09 14:53:20 -0300

    fixing migration problem with photo on schools table

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180709172529) do
+ActiveRecord::Schema.define(version: 20180709175210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "photos", force: :cascade do |t|
-    t.string "url"
-    t.bigint "school_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["school_id"], name: "index_photos_on_school_id"
-  end
 
   create_table "reviews", force: :cascade do |t|
     t.string "content"
@@ -43,8 +35,8 @@ ActiveRecord::Schema.define(version: 20180709172529) do
     t.float "latitude"
     t.float "longitude"
     t.string "city"
-    t.string "photo"
     t.decimal "rating_average"
+    t.string "photo"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,7 +57,6 @@ ActiveRecord::Schema.define(version: 20180709172529) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "photos", "schools"
   add_foreign_key "reviews", "schools"
   add_foreign_key "reviews", "users"
 end
